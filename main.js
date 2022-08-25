b4fa9445759e4bcdd78edfb633f9a0b754532b18
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-25 17:51:56 +0300

    done

--- main.js
+++ main.js
@@ -3,7 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', ()=>{
+const errorMessage = document.querySelector('#modal-message')
+const removeHidden = document.querySelector('#modal')
+const heartClick = document.querySelectorAll('.like-glyph')
 
+heartClick.forEach(element => {
+  element.addEventListener('click', () => {
+    //Invoke mimicServerCall to simulate making a server request
+    mimicServerCall()
+    .then(() => {
+      if (element.innerHTML === EMPTY_HEART){ //When the "server" returns a success status:
+        element.innerHTML = FULL_HEART
+        element.className = 'activated-heart'
+      } else {
+        element.innerHTML = EMPTY_HEART //Change the heart back to an empty heart
+        element.className = 'activated-heart'
+      }
+    })
+    //When the "server" returns a failure status:
+    .catch(error => {
+      removeHidden.classList.remove('hidden')
+      errorMessage.innerHTML = error
+      setTimeout(() => {
+        removeHidden.classList.add('hidden')
+        errorMessage.innerHTML = ''
+      }, 3000)  //3000 is in milliseconds
+    })
+  })
+})
+
+
+})
 
 
 
